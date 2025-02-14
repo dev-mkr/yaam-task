1de50f8b5ff524563d526dd54805ede4d8fe2744
dev-mkr <webdev.mu@gmail.com>
2025-02-14 19:59:28 +0200

    feat: created Table pagination component

---
+++ src/components/shared/Table/components/TablePagination.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+import clsx from "clsx";
+import { Button } from "../../ui/Button";
+
+interface Props {
+  page: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+  className?: string;
+}
+
+export const TablePagination: FC<Props> = ({
+  page,
+  totalPages,
+  onPageChange,
+  className,
+}) => {
+  return (
+    <div
+      className={clsx(
+        "container mx-auto flex max-w-7xl items-center justify-between px-4 py-3",
+        className
+      )}
+    >
+      <p>
+        Page {page} of {totalPages}
+      </p>
+      <div className="flex gap-2">
+        <Button onClick={() => onPageChange(page - 1)} disabled={page === 1}>
+          Previous
+        </Button>
+        <Button
+          onClick={() => onPageChange(page + 1)}
+          disabled={page >= totalPages}
+        >
+          Next
+        </Button>
+      </div>
+    </div>
+  );
+};
