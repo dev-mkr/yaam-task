f07315e9b8e84a6f4c9c58ebf9e7cf0f4cf2759e
dev-mkr <webdev.mu@gmail.com>
2025-02-14 19:56:16 +0200

    feat: add error boundary to Layout component

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,13 +1,17 @@
+import { ErrorBoundary } from "react-error-boundary";
 import { Sidebar } from "./Sidebar";
 import { Toaster } from "react-hot-toast";
 import { Outlet } from "react-router-dom";
+import { GlobalError } from "@/components/shared/GlobalError";
 
 export const Layout = () => {
   return (
     <div className="flex flex-col gap-x-5 bg-gray-50 md:flex-row">
       <Sidebar />
-      <Outlet />
-      <Toaster position="top-right" />
+      <ErrorBoundary fallback={<GlobalError />}>
+        <Outlet />
+      </ErrorBoundary>
+      <Toaster position="top-center" />
     </div>
   );
 };
